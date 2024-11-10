39d909efc8f8e7b9acf951244220d345a0c3d080
sonhmai <14060682+sonhmai@users.noreply.github.com>
2024-11-10 14:38:50 +0700

    implement Schema

--- java/src/main/java/com/simpledb/record/Schema.java
+++ java/src/main/java/com/simpledb/record/Schema.java
@@ -1,7 +1,12 @@
 package com.simpledb.record;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Schema contains a record's schema.
+ * Schema contains a record's schema (logical info). Physical info is in Layout.
  * 1. name and type of each field
  * 2. length of each string field
  * <p>
@@ -9,9 +14,15 @@ package com.simpledb.record;
  * not its size in bytes.
  */
 public class Schema {
+  private final List<String> fields = new ArrayList<>();
+  private final Map<String, FieldInfo> info = new HashMap<>();
+
   public Schema() {}
 
-  public void addField(String fieldname, int type, int length) {}
+  public void addField(String fieldname, int type, int length) {
+    fields.add(fieldname);
+    info.put(fieldname, new FieldInfo(type, length));
+  }
 
   public void addIntField(String fieldname) {}
 
@@ -20,4 +31,25 @@ public class Schema {
   public void add(String fieldname, Schema sch) {}
 
   public void addAll(Schema sch) {}
+
+  public List<String> fields() {
+    return fields;
+  }
+
+  public boolean hasField(String fieldname) {
+    return info.containsKey(fieldname);
+  }
+
+  public int type(String fieldname) {
+    return info.get(fieldname).type;
+  }
+
+  class FieldInfo {
+    int type;
+    int length;
+    public FieldInfo(int type, int length) {
+      this.type = type;
+      this.length = length;
+    }
+  }
 }
